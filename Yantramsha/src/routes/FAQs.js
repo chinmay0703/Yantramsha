1540d3ac3eafe67d5205d82f94acf247619232a8
Chinmay Satish Vyawahare <chinya2103@gmail.com>
2023-12-06 13:51:04 +0530

    e

--- Yantramsha/src/routes/FAQs.js
+++ Yantramsha/src/routes/FAQs.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer/Footer';
 
 const FAQs = () => {
     const activeItem = "FAQs";
@@ -8,6 +10,11 @@ const FAQs = () => {
   return (
     <div className='App'>
         <Navbar activeItem={activeItem} setActiveItem={setActiveItem}  />
+        <div className='container'>
+
+            <h1>FAQs</h1>
+        </div>
+        <Footer></Footer>
     </div>
   )
 }
